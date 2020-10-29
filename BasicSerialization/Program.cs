d02591a1220997e6c06f37bc3f76bc7449cbad82
Dana Wilson <danalwil@outlook.com>
2020-10-29 10:12:49 -0400

    Add project files.

---
+++ BasicSerialization/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace BasicSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.ID = 123;
+            emp.Name = "Dana";
+            emp.SSNumber = 123456789;
+            emp.EntryDate = DateTime.Now;
+            Console.WriteLine($"Employee ID: {emp.ID} \nEmployee Nme: {emp.Name}\nEmployee SS#: {emp.SSNumber}\nDate Created: {emp.EntryDate}");
+
+            string FilePath = "C:/Temp/";
+            string FileName = "Dana.xml";
+            TextWriter writer = new StreamWriter(FilePath + FileName);
+
+            XmlSerializer ser = new XmlSerializer(typeof(Employee));
+
+            ser.Serialize(writer, emp);
+            writer.Close();
+            Console.ReadLine();
+        }
+    }
+}
